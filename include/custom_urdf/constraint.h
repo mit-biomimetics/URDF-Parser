a38d2d41ab9380222ea7e794cda8a1ba914d2309
MatthewChignoli <matthew.chignoli@gmail.com>
2024-02-02 14:02:29 -0500

    Remove axes as position specific member of constraint class

--- include/custom_urdf/constraint.h
+++ include/custom_urdf/constraint.h
@@ -33,12 +33,8 @@ namespace urdf
         // Position constraint specific members
         //  The transform from the predecessor link frame to the constraint frame
         std::shared_ptr<Pose> predecessor_to_constraint_origin_transform;
-        //  The axes in the predecessor frame along which the constraint is enforced
-        std::shared_ptr<Vector3> predecessor_axis;
         //  The transform from the successor link frame to the constraint frame
         std::shared_ptr<Pose> successor_to_constraint_origin_transform;
-        //  The axes in the successor frame along which the constraint is enforced
-        std::shared_ptr<Vector3> successor_axis;
 
         // Rolling constraint specific members
         //  The ratio of the radius of the rolling constraint of the predecessor link to the radius 
@@ -46,16 +42,6 @@ namespace urdf
         std::shared_ptr<double> ratio;
 
         // TODO(@MatthewChignoli): add stuff for the differential constraint
-
-        std::vector<std::shared_ptr<Link>> allLinks() const
-        {
-            std::vector<std::shared_ptr<Link>> links;
-            links.insert(links.end(), nca_to_predecessor_subtree.begin(),
-                         nca_to_predecessor_subtree.end());
-            links.insert(links.end(), nca_to_successor_subtree.begin(),
-                         nca_to_successor_subtree.end());
-            return links;
-        }
 
         void clear()
         {
@@ -65,9 +51,7 @@ namespace urdf
             this->nca_to_predecessor_subtree.clear();
             this->nca_to_successor_subtree.clear();
             this->predecessor_to_constraint_origin_transform.reset();
-            this->predecessor_axis.reset();
             this->successor_to_constraint_origin_transform.reset();
-            this->successor_axis.reset();
             this->ratio.reset();
             this->type = UNKNOWN;
         };
